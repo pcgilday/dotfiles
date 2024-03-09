73aee9938405b7555ca470ab3bf6955f34d39217
Patrick Gilday <patrick@gildayhq.com>
2024-03-09 14:27:24 -0500

    chore: cleanup link-files

--- link-files.sh
+++ link-files.sh
@@ -7,8 +7,7 @@ ln -s "$HOME/dotfiles/.gitignore-global" "$HOME/.gitignore-global"
 ln -s "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
 ln -s "$HOME/dotfiles/.vim" "$HOME/.vim"
 ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
-# Put nvim dir in .config - contains init script which uses standard 
-# vim bits like vimrc and .vim
+# Put nvim dir in .config (linux/mac)
 ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 # ln -s "$HOME/dotfiles/.zprofile" "$HOME/.zprofile "
 
